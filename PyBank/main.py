c909eece52b6b0c446dbf5f0716bf0e3efe833bc
dczalewski <72892775+dczalewski@users.noreply.github.com>
2020-12-07 15:07:55 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,11 @@
 import os
 import csv
+totalyears=-1
 pybankcsv=os.path.join("Resources", "budget_data.csv")
 print("Financial Analysis")
 print("--------------------------------")
 with open(pybankcsv) as csvfile
     csvreader=csv.reader(csvfile, delimiter=",")
-    
+    totalyears=totalyears+len(list(budget_data))
+    print("Total months: " + totalyears)
 
